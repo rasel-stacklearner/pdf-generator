0747bd093f24ad8d1d98ecdcc7ecee9f11d66c72
Rasel Hossain <rasel@stacklearner.com>
2025-01-11 15:00:06 +0600

    update

--- apps/express-app/src/routes/invoice.ts
+++ apps/express-app/src/routes/invoice.ts
@@ -1,4 +1,4 @@
-import { CreateInvoiceSchema, CreateInvoiceItemSchema } from "@repo/db";
+import { CreateInvoiceSchema, CreateInvoiceItemSchema } from "@repo/db/invoice";
 import { InvoiceService, InvoiceRepository} from "@repo/core/invoice";
 import { Router, Request, Response, NextFunction } from "express";
 import { z } from "zod";
