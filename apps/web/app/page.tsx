0fffddd2a0b034f48c124d85bd1fc6b78c1aeb85
Rasel Hossain <rasel@stacklearner.com>
2025-01-11 18:37:14 +0600

    update

--- apps/web/app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-export default function HomePage() {
-  return (
-    <main className="min-h-screen">
-     <h1>Welcome to the Home Page</h1>
-    </main>
-  );
-}
--- apps/web/config/about/stats.ts
+++
@@ -1,6 +0,0 @@
-export const stats = [
-	{ label: 'Happy Customers', value: '10K+' },
-	{ label: 'Products Sold', value: '25K+' },
-	{ label: 'Years of Experience', value: '5+' },
-	{ label: 'Brand Partners', value: '20+' },
-];
