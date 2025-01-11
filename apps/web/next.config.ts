dfd76c0493f063225b003e933c921ca8dc956349
Rasel Hossain <rasel@stacklearner.com>
2025-01-11 18:08:16 +0600

    update

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -4,8 +4,6 @@ const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ["@repo/ui"],
 
-  basePath: "",
-  assetPrefix: "/about",
 };
 
 export default nextConfig;
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,18 +1,1 @@
-import type { Config } from "tailwindcss";
-
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
-} satisfies Config;
+export * from "@repo/tailwind-config/web"
