aea45e12779e3f4a47e839714c21dbf61540a8b3
Rasel Hossain <rasel@stacklearner.com>
2025-01-11 23:27:06 +0600

    update

--- packages/date/src/date.ts
+++ packages/date/src/date.ts
@@ -15,9 +15,9 @@ export const subtractDaysFromDate = (date: Date | string, days: number): Date =>
   return subDays(dateObj, days);
 };
 
-export const isValidDate = (date: any): boolean => {
-  if (typeof date === 'string') {
-    return isValid(parseISO(date));
-  }
-  return isValid(date);
-};
+// export const isValidDate = (date: any): boolean => {
+//   if (typeof date === 'string') {
+//     return isValid(parseISO(date));
+//   }
+//   return isValid(date);
+// };
