29400b18b7494dd5adef8b33dece35c3e8df9993
Rasel Hossain <rasel@stacklearner.com>
2025-01-12 16:16:09 +0600

    update

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,8 @@
 
-import Button from "@repo/ui/components/Button"
 export default function HomePage() {
   return (
     <main className="min-h-screen">
      <h1>Welcome to the Home Page</h1>
-     <Button>Click me</Button>
     </main>
   );
 }
